6f93631a5b746bff08386b65ab5c9f4f23971d45
adamyrzakanova <aidaimyrzakanova@usf.edu>
2025-01-26 21:29:12 -0500

    Task 1 - Employee Information.

---
+++ cc_1.js
@@ -0,0 +1,7 @@
+let employeeName = "name_of_employee";
+const employeeId = "id_of_employee";
+var isActive = true;
+
+console.log(typeof employeeName);
+console.log(typeof employeeId);
+console.log(typeof isActive); 
