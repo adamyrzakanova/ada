0c51d60e6b74243b8c4efacd963bd72fa10379de
adamyrzakanova <aidaimyrzakanova@usf.edu>
2025-01-26 21:30:42 -0500

    Task 2 - Product Details.

--- cc_1.js
+++ cc_1.js
@@ -5,3 +5,12 @@ var isActive = true;
 console.log(typeof employeeName);
 console.log(typeof employeeId);
 console.log(typeof isActive); 
+
+// task 2 
+let productName = "name_of_product";
+const productPrice = "100"; 
+var isAvailable = true;
+
+console.log(typeof productName);
+console.log(typeof productPrice);   
+console.log(typeof isAvailable);
